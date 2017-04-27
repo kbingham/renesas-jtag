09c7576f453adfe4fba9c4bd79404caf71275d48
Jacopo Mondi <jacopo@jmondi.org>
2017-04-27 19:43:11 +0200

    Add support for Genmai board
    Add support for genmai board and r7s72100 SoC
    
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>

---
+++ target/renesas_rz.tcl
@@ -0,0 +1,21 @@
+# Common Renesas RZ config file
+
+# Coresight Debug Access Port
+if { [info exists DAP_TAPID] } {
+	set _DAP_TAPID $DAP_TAPID
+} else {
+	set _DAP_TAPID 0x4ba00477
+}
+
+adapter_khz 300
+
+proc init_reset {mode} {
+    # Assert both resets: equivalent to a power-on reset
+    jtag_reset 1 1
+
+    # Deassert TRST to begin TAP communication
+    jtag_reset 0 1
+
+    # TAP should now be responsive, validate the scan-chain
+    jtag arp_init
+}
